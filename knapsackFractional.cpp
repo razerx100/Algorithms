d1ed84c474a41016bcba172b9ab2d65cd89eb168
Saikat <saikatmandal8@gmail.com>
2020-09-07 03:05:53 +0600

    added bit more in knapsack

--- knapsackFractional.cpp
+++ knapsackFractional.cpp
@@ -20,7 +20,7 @@ void sort_old_position(float* solutions, Value* weight, int size){
         solutions[i] = new_solutions[i];
     }
 }
-void knapsack(int size, int objects_number, Value* weight){
+void knapsack(int size, int objects_number, Value* weight, Value* profit){
     float solutions[objects_number], SIZE = size;
     for(int i = 0; i < objects_number; i++){
         solutions[i] = 0.0f;
@@ -38,11 +38,20 @@ void knapsack(int size, int objects_number, Value* weight){
     if(i <= objects_number){
         solutions[i] = SIZE / weight[i].value;
     }
+    float sum_of_weight = 0.0, sum_of_profits = 0.0;
+    for(int i = 0; i < objects_number; i++){
+        sum_of_weight += solutions[i] * weight[i].value;
+        sum_of_profits += solutions[i] * profit[i].value;
+    }
+
     sort_old_position(solutions, weight, objects_number);
+    cout << "Fractions : ";
     cout << solutions[0];
     for(int i = 1; i < objects_number; i++){
         cout << ", " << solutions[i];
     }
+    cout << "\nSum of profits : " << sum_of_profits << "\n";
+    cout << "Sum of weights : " << sum_of_weight << "\n";
 }
 void swap(int &a, int &b){
     int temp = a;
@@ -83,7 +92,6 @@ int main(){
     cout << "Weights : ";
     print_array(number, w);
     sort(number, p, w);
-    cout << "Fractions : ";
-    knapsack(limit, number, w);
+    knapsack(limit, number, w, p);
     return 0;
 }
